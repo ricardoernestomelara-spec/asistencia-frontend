9a29486a59f07ca39cd6ba9e6d5c32b1c8ea98d8
ricardo <ricardo.ernesto.melara@clases.edu.sv>
2026-09-18 07:46:13 -0600

    Forzar URL de Render fija

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // src/config.js
-export const API_BASE = import.meta.env.VITE_API_URL || 'https://asistencia-backend-qgim.onrender.com/api';
+
+
+export const API_BASE = 'https://asistencia-backend-qgim.onrender.com/api';
