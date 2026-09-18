4edcdbd48e26d79e5a716e57a267c9716fe58de6
ricardo <ricardo.ernesto.melara@clases.edu.sv>
2026-09-18 07:14:52 -0600

    Corregido nombre de constante a API_BASE en config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
+
+
 // src/config.js
-export const API_URL = import.meta.env.VITE_API_URL || 'https://asistencia-backend-qgim.onrender.com/api';
+export const API_BASE = import.meta.env.VITE_API_URL || 'https://asistencia-backend-qgim.onrender.com/api';
